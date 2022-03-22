d7b9d8d43ffdba3e49cf41351a94926c5b9df9a0
jbennett-1 <juliabennett46@gmail.com>
2022-03-21 17:23:39 -0700

    updated all files

--- m4code/src/test.c
+++ m4code/src/test.c
@@ -1,18 +1,47 @@
 /* test program */
-typedef struct 
-{
+#include ""://update later
+
+//states
+#define IDLE_STATE 0
+#define RUN_STATE 1
+#define STOP_STATE 2
+
+//functions
+void SystemInit(void);
+void SystemCoreClockUpdate(void);
+void DisplayTime(void);
+void SetClockFreq(void);
+void SysTick_Handler(void);
+ 
+//variables
+uint32_t SysTick_Config(uint32_t ticks) /* SysTick */
+uint32_t SystemCoreClock;
+volatile int CurrState;
+volatile unsigned long TickCounter;
+volatile int userInput;
+
+void main(void){
+	SystemInit();
+	SystemCoreClockUpdate();
+	
+	uint32_t returnC;
+	returnC=SysTick_Config(SystemCoreClockUpdate/1000); //generate interrupt every ms
+
+	if(returnC!=0){
+	//error functions
+	}
+}
+
+typedef struct {
 	volatile unsigned long CTRL;
 	volatile unsigned long LOAD;
 	volatile unsigned long VAL;
 	volatile unsigned long CALIB; 
-} SysTick_Type;
+} SysTick_Type; //for SysTick config
 
-#define SysTick((SysTick_Type* )*0xE000E010) /*SysTick struct */
-
-SysTick->VAL=0x0;
-SysTick->LOAD=0xFFFFF; //clock cycles/reload val
-SysTick->CTRL=0x5;
-
+SysTick->VAL=0; //clear current value to 0
+SysTick->LOAD=0x1023; //count down from 1023
+SysTick->CTRL=0x7;
 
 #if 0
 void _start(){
